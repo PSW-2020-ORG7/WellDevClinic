a7eb04b6c2abdcf343ed3955ab7626edc76f19db
Andjela Petrovic <petrovic.andjela755@gmail.com>
2020-11-20 01:07:50 +0100

    #frontend view

---
+++ PSW_Web_app/wwwroot/js/viewDocuments.js
@@ -0,0 +1,31 @@
+﻿$(document).ready(function () {
+
+	let type = $('select[name="type"]').val();
+	$('input[name="specialist"]').attr('disabled', true);
+	$('input[name="drug"]').attr('disabled', true);
+	$('select[name="type"]').change(function () {
+		var value = $(this).val();
+		if (value == "Refferal") {
+			$(('input[name="specialist"]')).attr('disabled', false);
+			$('input[name="drug"]').attr('disabled', true);
+			$('input[name="drug"]').val("");
+		}
+		if (value == "Prescription") {
+			$('input[name="drug"]').attr('disabled', false);
+			$('input[name="specialist"]').attr('disabled', true);
+			$('input[name="specialist"]').val("");
+		}
+		if (value == "none") {
+			$('input[name="specialist"]').attr('disabled', true);
+			$('input[name="drug"]').attr('disabled', true);
+			$('input[name="specialist"]').val("");
+			$('input[name="drug"]').val("");
+		}
+	});
+
+	$('input[type="button"]').click(function (event) {
+		$('input[name="specialist"]').attr('disabled', true);
+		$('input[name="drug"]').attr('disabled', true);
+		document.forms["form"].reset();
+	});
+});
