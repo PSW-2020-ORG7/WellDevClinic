85997637f0890cf519fdafbb247a32c37c26f0df
tasa1998 <tasa1998@uns.ac.rs>
2020-11-20 21:10:11 +0100

    #BugFix added empty RoomType constructor

---
+++ PSW_Pharmacy_Adapter/Configuration/APIConfig.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace PSW_Pharmacy_Adapter.Configuration
+{
+    public static class APIConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Web API routes
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+        }
+    }
+}
