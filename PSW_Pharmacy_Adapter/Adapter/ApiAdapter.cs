e1c14d5521339e4b9f851fe4f0e865809abdd8dd
tasa1998 <tasa1998@uns.ac.rs>
2020-11-22 16:08:00 +0100

    #feature HTTP get request

---
+++ PSW_Pharmacy_Adapter/Adapter/ApiAdapter.cs
@@ -0,0 +1,22 @@
+﻿using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PSW_Pharmacy_Adapter.Adapter
+{
+    public class ApiAdapter
+    {
+
+        private static void SendGetRequestWithRestSharp()
+        {
+            var client = new RestSharp.RestClient("http://localhost:4200");
+            var request = new RestRequest("/api");
+            var response = client.Get<List<DTO.ApiDTO>>(request);
+            //Console.WriteLine("Status: " + response.StatusCode.ToString());
+            List<DTO.ApiDTO> result = response.Data;
+            result.ForEach(api => Console.WriteLine(api.ToString()));
+        }
+    }
+}
---
+++ PSW_Pharmacy_Adapter/DTO/ApiDTO.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Text;
+
+namespace PSW_Pharmacy_Adapter.DTO
+{
+    public class ApiDTO
+    {
+        public string api { get; set; }
+        public string url { get; set; }
+
+        public ApiDTO() { }
+
+        public override string ToString()
+        {
+            return this.api;
+        }
+    }
+}
