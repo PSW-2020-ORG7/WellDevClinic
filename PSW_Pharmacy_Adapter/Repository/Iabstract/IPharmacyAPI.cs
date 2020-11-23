7d7e246c65473d8c803ab568b2df5909905a3ad7
tasa1998 <tasa1998@uns.ac.rs>
2020-11-23 23:27:57 +0100

    #feature tests

---
+++ PSW_Pharmacy_Adapter/Repository/Iabstract/IPharmacyAPI.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using bolnica.Repository;
+
+namespace PSW_Pharmacy_Adapter.Repository.Iabstract
+    {
+    public interface IPharmacyAPI
+    {
+
+    }
+}
---
+++ PSW_Pharmacy_Adapter/UnitTests/Manipulate_Pharmacy_API_test.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PSW_Pharmacy_Adapter.UnitTests
+{
+    public class Manipulate_Pharmacy_API_test
+    {
+    }
+}
