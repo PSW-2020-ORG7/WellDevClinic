8cf4297555a36e86e45d77ea72e67a414e0a90ab
gojko1001 <57567042+gojko1001@users.noreply.github.com>
2020-11-29 04:02:09 +0100

    [Frontend] Added table of partner pharmacies to index page

---
+++ PSW_Pharmacy_Adapter/wwwroot/js/index.js
@@ -0,0 +1,26 @@
+﻿$(document).ready(function () {
+    $.ajax({
+        method: "GET",
+        url: "../api/apikey/all",
+        contentType: "application/json",
+        success: function (apiDb) {
+            if (apiDb) {
+                viewApis(apiDb)
+            }
+        },
+    });
+});
+
+function viewApis(apiDb) {
+    for (api of apiDb) {
+        content = "<tr><td>";
+        content += api.nameOfPharmacy;
+        content += "</td><td>";
+        content += api.apiKey;
+        content += "</td><td>";
+        content += api.url;
+        content += "</td></tr>";
+
+        $("#apiTable").append(content);
+    }
+}
