0e35a913a75df5578cf45ef2307b3b5be7f9e86f
tasa1998 <tasa1998@uns.ac.rs>
2020-11-29 01:53:13 +0100

    [Frontent] Start page for integration

---
+++ PSW_Pharmacy_Adapter/wwwroot/js/startPage.js
@@ -0,0 +1,13 @@
+﻿$(document).ready(function () {
+    $.ajax({
+        method: "GET",
+        url: "../api/apikey/all",
+        contentType: "application/json",
+        data: jsonApi,
+        success: function (data) {
+            if (data) {
+                alert("Succesfully loaded from database");
+            }
+        },
+    });
+});
