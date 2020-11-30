a1bb416582bb3760e894c06ff889fc52da6ed21a
tasa1998 <tasa1998@uns.ac.rs>
2020-11-30 03:44:58 +0100

    [Feature] sftp request with rebex

---
+++ PSW_Pharmacy_Adapter/Model/Sftp.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PSW_Pharmacy_Adapter.Model
+{
+    public class Sftp
+    {
+        public string Host { get; set; }
+        public int Port { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+    }
+}
