8a9e09ea8865911a525f27f44cd09fdc8167d85b
tasa1998 <tasa1998@uns.ac.rs>
2020-11-30 22:16:56 +0100

    [BugFix]

---
+++ ServiceTests/Pharmacy_Adapter_Tests/Sftp_Test.cs
@@ -0,0 +1,24 @@
+﻿using PSW_Pharmacy_Adapter.Service;
+using Renci.SshNet;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace ServiceTests.Pharmacy_Adapter_Tests
+{
+    public class Sftp_Test
+    {
+
+        //[Fact]
+        //public void sendFile()
+        //{
+        //    SftpService service = new SftpService(new SftpClient("192.168.0.16", 22, "user", "password"));
+
+        //    bool isSuccess = service.UploadFileToSftpServer(@"C:\Users\Nastasja\Desktop\psw projekat\WellDevClinic\PSW_Pharmacy_Adapter\wwwroot\primer.txt");
+        //    Assert.True(isSuccess);
+        //}
+        
+
+    }
+}
