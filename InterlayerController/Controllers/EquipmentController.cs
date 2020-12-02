c16fcd688b124aa56b54538b41eb5d1e51543784
Kristina Djuric <kristinadjuriic@gmail.com>
2020-12-02 22:22:30 +0100

    server side implementation

---
+++ InterlayerController/Controllers/EquipmentController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Model.Director;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InterlayerController.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EquipmentController : ControllerBase
+    {
+        private bolnica.Controller.IEquipmentController _equipmentController = new bolnica.Controller.EquipmentController();
+
+        /// <summary>
+        ///calls GetAll() method from class EquipmentController 
+        ///so it can get all equipment from database
+        /// </summary>
+        /// <returns>status 200 OK response with a list of equipment</returns>
+        [HttpGet]
+
+        public IEnumerable<Equipment> GetAllEquipment()
+        {
+            List<Equipment> result = (List<Equipment>)_equipmentController.GetAll();
+
+            return result;
+        }
+    }
+}
