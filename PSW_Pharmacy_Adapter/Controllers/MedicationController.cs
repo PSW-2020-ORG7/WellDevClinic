8ff422cdf35f656bea6560fd7073da48eefd0f3b
tasa1998 <tasa1998@uns.ac.rs>
2020-12-06 15:40:08 +0100

    [FeatureUpdate] get medications from hospital database

---
+++ PSW_Pharmacy_Adapter/Controllers/MedicationController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using PSW_Pharmacy_Adapter.Model;
+using PSW_Pharmacy_Adapter.Service;
+
+namespace PSW_Pharmacy_Adapter.Controllers
+{
+    [Route("api/[controller]")]
+    public class MedicationController : ControllerBase
+    {
+
+        private readonly MedicationService _medicationService;
+
+        public MedicationController(MedicationService medicationService) 
+        {
+            _medicationService = medicationService;
+        }
+        [HttpGet]
+        [Route("getAll")]
+        public async Task<IActionResult> getHospitalsMedicationStockAsync()
+        {
+            return Ok(await _medicationService.GetAllMedication());
+        }
+    }
+}
---
+++ PSW_Pharmacy_Adapter/Service/MedicationService.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json;
+using PSW_Pharmacy_Adapter.Model;
+using PSW_Pharmacy_Adapter.Service.Iabstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace PSW_Pharmacy_Adapter.Service
+{
+    public class MedicationService 
+    {
+        static readonly HttpClient client = new HttpClient();
+
+        public async Task<List<Medication>> GetAllMedication()
+        {
+            HttpResponseMessage response = await client.GetAsync("https://localhost:44375/api/drug");
+            response.EnsureSuccessStatusCode();
+            string responseBody = await response.Content.ReadAsStringAsync();
+            List<Medication> medications = JsonConvert.DeserializeObject<List<Medication>>(responseBody);
+            medications.ForEach(medication => Console.WriteLine(medication.ToString()));
+            return medications;
+        }
+
+    }
+}
