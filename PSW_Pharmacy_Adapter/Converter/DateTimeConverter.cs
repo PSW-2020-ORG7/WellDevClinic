9661c88df97b68c8dd0b86c5b464aa1c4c3e32e0
marijamilanovic <marijamil@uns.ac.rs>
2020-12-07 14:06:04 +0100

    [BugFix] fixed error with dates in ActionsAndBenefits

---
+++ PSW_Pharmacy_Adapter/Converter/DateTimeConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Newtonsoft.Json;
+using PSW_Pharmacy_Adapter.Dto;
+
+namespace PSW_Pharmacy_Adapter.Converter
+{
+    public class DateTimeConverter
+    {
+        private static readonly DateTime epoch = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+
+        public static DateTime FromUnixTime(long unixTime)
+           => epoch.AddSeconds(unixTime);
+
+        public static ActionAndBenefitDtoConverted DtoToModel(ActionAndBenefitDto actionAndBenefitDto)
+            => new ActionAndBenefitDtoConverted(actionAndBenefitDto.Id,
+                                                    actionAndBenefitDto.PharmacyName,
+                                                    actionAndBenefitDto.MessageAboutAction,
+                                                    DateTimeOffset.FromUnixTimeMilliseconds(actionAndBenefitDto.StartDate).DateTime,
+                                                    DateTimeOffset.FromUnixTimeMilliseconds(actionAndBenefitDto.EndDate).DateTime);
+    }
+}
