53650eddc2913630faf414506b0f0783fc4ac8df
jovan-jelicki <jovanzte@gmail.com>
2020-12-09 10:42:38 +0100

    Modified dockerFiles and WebbApp program.cs

--- PSW_Web_app/Program.cs
+++ PSW_Web_app/Program.cs
@@ -1,4 +1,5 @@
 using System;
+using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
 
@@ -10,26 +11,20 @@ namespace PSW_Web_app
         {
             CreateHostBuilder(args).Build().Run();
         }
-        private static int calculatePort()
-        {
-            var port = System.Environment.GetEnvironmentVariable("PORT");
-            if (port == null)
-                return 49153;
-            else
-                return Int32.Parse(System.Environment.GetEnvironmentVariable("PORT"));
-        }
-
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
-                    webBuilder.ConfigureKestrel(serverOptions =>
-                    {
-                        serverOptions.ListenAnyIP(calculatePort());
-                    })
-                    .UseStartup<Startup>();
-
+                    webBuilder.UseStartup<Startup>();
                 });
+        // heroku port variable
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>()
+                .UseKestrel(options =>
+             {
+            options.ListenAnyIP(Int32.Parse(System.Environment.GetEnvironmentVariable("PORT")));
+        });
     }
 }
 
