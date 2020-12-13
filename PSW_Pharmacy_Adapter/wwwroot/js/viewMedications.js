dda16d98e4c284df4516241573714402f92d60ec
marijamilanovic <marijamil@uns.ac.rs>
2020-12-13 22:04:04 +0100

    [Frontend] frontend for medicines and prescriptions

---
+++ PSW_Pharmacy_Adapter/wwwroot/js/viewMedications.js
@@ -0,0 +1,35 @@
+﻿$(document).ready(function () {
+    $.ajax({
+        method: "GET",
+        url: "../api/medication",
+        contentType: "application/json",
+        success: function (data) {
+            console.log(data);
+			getAllMedications(data);
+			$(".loader").css("display", "none");
+        },
+    });
+});
+
+function getAllMedications(data) {
+	$("#viewMedication").empty();
+	for (let med of data) {
+		let content = '<div class="card" id="' + med.id + '">';
+		content += '<div class="card-body">';
+		content += '<div class="data">';
+		content += '<table style="margin:10px">';
+		content += '<tr><td float="right">Id:</td><td>';
+		content += med.id;
+		content += '</td></tr>';
+		content += '<tr><td float="right">Name:</td><td>';
+		content += med.name;
+		content += '</td></tr>';
+		content += '<tr><td float="right">Amount:</td><td>';
+		content += med.amount;
+		content += '</td></tr>';
+		content += '</table>';
+		content += '</div></div></div>';
+		content += '</div>';
+		$("#viewMedication").append(content);
+	}
+}
---
+++ PSW_Pharmacy_Adapter/wwwroot/js/viewPrescription.js
@@ -0,0 +1,41 @@
+﻿$(document).ready(function () {
+	$.ajax({
+		method: "GET",
+		url: "../api/prescription",
+		contentType: "application/json",
+		success: function (data) {
+			console.log(data);
+			$(".loader").css("display", "none");
+			getAllPrescriptions(data);
+		},
+	});
+});
+
+function getAllPrescriptions(data) {
+	$("#viewPrescription").empty();
+	for (let pre of data) {
+		var content = '<div class="card" id="' + pre.id + '">';
+		content += '<div class="card-body">';
+		content += '<div class="data">';
+		content += '<table style="margin:10px">';
+		content += '<tr><td float="right">Id:</td><td>';
+		content += pre.id;
+		content += '</td></tr>';
+		content += '<tr><td float="right">Start date:</td><td>';
+		content += pre.period.startDate;
+		content += '</td></tr>';
+		content += '<tr><td float="right">End date:</td><td>';
+		content += pre.period.endDate;
+		content += '</td></tr>';
+		//content += '<tr><td float="right">Prescription medicines:</td><td>';
+		//var c;
+		//for (c = 0; c < data.medication.length; c++) {
+			//content += data.medication[c].name;
+		//}
+		//content += '</td></tr>';
+		content += '</table>';
+		content += '</div></div></div>';
+		content += '</div>';
+		$("#viewPrescription").append(content);
+	}
+}
