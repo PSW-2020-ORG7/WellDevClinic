982aa0245cb9684fe84081a24fecc2d5df015f6f
Kristina Djuric <kristinadjuriic@gmail.com>
2020-12-27 22:56:20 +0100

    refactoring and fixing bugs

--- PSW_Wpf_app/ViewModel/EmergencyAppointmentViewModel.cs
+++ PSW_Wpf_app/ViewModel/EmergencyAppointmentViewModel.cs
@@ -9,7 +9,8 @@ namespace PSW_Wpf_app.ViewModel
 {
     public class EmergencyAppointmentViewModel : BindableBase
     {
-        BindingList<Equipment> equipments = new BindingList<Equipment>();
+        private BindingList<Equipment> equipments = new BindingList<Equipment>();
+        private BindingList<ExaminationDTO> examinations = new BindingList<ExaminationDTO>();
 
         public BindingList<Equipment> Equipments
         {
@@ -23,7 +24,6 @@ namespace PSW_Wpf_app.ViewModel
                 OnPropertyChanged("Equipments");
             }
         }
-        private BindingList<ExaminationDTO> examinations = new BindingList<ExaminationDTO>();
         public BindingList<ExaminationDTO> Examinations
         {
             get
@@ -54,35 +54,13 @@ namespace PSW_Wpf_app.ViewModel
         private async void LoadTerms(int selectedType, Equipment equipment)
         {
             List<Doctor> doctors = new List<Doctor>(await WpfClient.GetAllDoctors());
-            List<Doctor> generalPractice = new List<Doctor>();
-            List<Doctor> specialists = new List<Doctor>();
-
-            foreach (Doctor d in doctors)
-            {
-                if (d.Specialty.Name.Equals("general practice"))
-                {
-                    generalPractice.Add(d);
-                }
-                else
-                {
-                    specialists.Add(d);
-                }
-            }
             
-
-            if (selectedType == 0)
-            {
-                doctors = generalPractice;
-            }
-            else
-            {
-                doctors = specialists;
-            }
+            doctors = GetDoctorsByType(doctors, selectedType);
 
             PriorityType priority = PriorityType.NoPriority;
             Period period = new Period();
             period.StartDate = DateTime.Now;
-            period.EndDate = period.StartDate.AddHours(2);
+            period.EndDate = period.StartDate.AddHours(1);
             List<ExaminationDTO> allTerms = new List<ExaminationDTO>();
             foreach (Doctor doctor in doctors)
             {
@@ -96,7 +74,16 @@ namespace PSW_Wpf_app.ViewModel
             }
 
             List<ExaminationDTO> validTerms = findValidRooms(equipment, allTerms);
+            ExaminationDTO examination = findEmergencyTerm(validTerms, period);
 
+            if (examination == null)
+                MessageBox.Show("There are no free terms!");
+
+            Examinations.Add(examination);
+        }
+
+        private ExaminationDTO findEmergencyTerm(List<ExaminationDTO> validTerms, Period period)
+        {
             ExaminationDTO exam = new ExaminationDTO();
             DateTime min = period.EndDate;
             foreach (ExaminationDTO ex in validTerms)
@@ -110,11 +97,28 @@ namespace PSW_Wpf_app.ViewModel
                     }
                 }
             }
-            if(exam == null)
+
+            return exam;
+        }
+
+        private List<Doctor> GetDoctorsByType(List<Doctor> doctors, int selectedType)
+        {
+            List<Doctor> generalPractice = new List<Doctor>();
+            List<Doctor> specialists = new List<Doctor>();
+
+            foreach (Doctor d in doctors)
             {
-                MessageBox.Show("There are no free terms!");
+                if (d.Specialty.Name.Equals("general practice"))
+                     generalPractice.Add(d);
+                else
+                    specialists.Add(d);
             }
-            Examinations.Add(exam);
+
+            if (selectedType == 0)
+                return generalPractice;
+            else
+                return specialists;
+            
         }
 
         private List<ExaminationDTO> findValidRooms(Equipment equipment, List<ExaminationDTO> allTerms)
@@ -123,14 +127,12 @@ namespace PSW_Wpf_app.ViewModel
             foreach (ExaminationDTO term in allTerms)
             {
                 if (term.Room.Equipment_inventory.Find(x => x.Equipment.Name == (equipment.Name)) != null)
-                {
                     examinations.Add(term);
-                }
             }
+
             if (examinations.Count == 0)
-            {
                 MessageBox.Show("There is no selected equipment in any of rooms in hospital!");
-            }
+
             return examinations;
         }
 
