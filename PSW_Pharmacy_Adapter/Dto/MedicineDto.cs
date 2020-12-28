fa306190b81a55a86411ca964a87e5baf5c3b754
majastamenic <stamenicmaja5@uns.ac.rs>
2020-12-28 16:56:37 +0100

    [FeatureUpdate] Mapper and dto for medications

--- PSW_Pharmacy_Adapter/Dto/MedicineDto.cs
+++ PSW_Pharmacy_Adapter/Dto/MedicineDto.cs
@@ -1,10 +1,21 @@
-﻿namespace PSW_Pharmacy_Adapter.Dto
+﻿using System;
+using System.Collections.Generic;
+
+namespace PSW_Pharmacy_Adapter.Dto
 {
     public class MedicineDto
     {
-        public string Name { get; set; }
         public long Id { get; set; }
+        public string Name { get; set; }
+        public string TypeOfMedicine { get; set; }
+        public string FormOfMedicine { get; set; }
+        public List<string> Ingredients { get; set; }
+        public string Manufactured { get; set; }
+        public string PublishingType { get; set; }
+        public List<string> ReplacementMedicines { get; set; }
+        public double Price { get; set; }
         public int Amount { get; set; }
+        public string Note { get; set; }
 
         public MedicineDto(){}
 
@@ -13,6 +24,22 @@
             Name = name;
             Id = id;
             Amount = amount;
+        }
+
+        public MedicineDto(long id, string name, string typeOfMedicine, string formOfMedicine, List<string> ingredients,
+            string manufactured, string publishingType, List<string> replacementMedicines, double price, int amount, string note)
+        {
+            Id = id;
+            Name = name;
+            TypeOfMedicine = typeOfMedicine;
+            FormOfMedicine = formOfMedicine;
+            Ingredients = ingredients;
+            Manufactured = manufactured;
+            PublishingType = publishingType;
+            ReplacementMedicines = replacementMedicines;
+            Price = price;
+            Amount = amount;
+            Note = note;
         }
     }
 }
