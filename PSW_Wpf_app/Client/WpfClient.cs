27f6cb5cfc5cfaf825d1299a8106691f4ef69a98
Maja18 <majadragojlovic98@gmail.com>
2021-01-17 23:38:47 +0100

    equipment refactoring

--- PSW_Wpf_app/Client/WpfClient.cs
+++ PSW_Wpf_app/Client/WpfClient.cs
@@ -252,7 +252,7 @@ namespace PSW_Wpf_app.Client
 
         public static async Task<List<Equipment>> GetAllEquipment()
         {
-            HttpResponseMessage response = await client.GetAsync("http://localhost:51393/api/equipment");
+            HttpResponseMessage response = await client.GetAsync("http://localhost:57400/api/equipment");
             response.EnsureSuccessStatusCode();
             string responseBody = await response.Content.ReadAsStringAsync();
             List<Equipment> equipments = JsonConvert.DeserializeObject<List<Equipment>>(responseBody);
