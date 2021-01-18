ecbc109721215ec8f8f385f813a1863e7c2b32a8
marijamilanovic <marijamil@uns.ac.rs>
2021-01-18 14:29:49 +0100

    [Tests] fixed test Add_Tender_Successfully

--- IntegrationTests/Pharmacy_Adapter_Tests/TenderTests.cs
+++ IntegrationTests/Pharmacy_Adapter_Tests/TenderTests.cs
@@ -13,12 +13,10 @@ namespace ServiceTests.Pharmacy_Adapter_Tests
     public class TenderTests
     {
         public const int TEST_ID = 11111111;
-        public DateTime TEST_START = DateTime.Now;
-        public DateTime TEST_END = DateTime.Now.AddDays(5);
         public long TEST_OFFER_WINNER = 0;
         public bool TEST_DELETED = false;
 
-        /*[Fact]
+        [Fact]
         public void Add_Tender_Successfully()
         {
             MyContextFactory cf = new MyContextFactory();
@@ -26,11 +24,11 @@ namespace ServiceTests.Pharmacy_Adapter_Tests
             var EmailServiceMock = new Mock<IPharmacyEmailsService>();
             TenderService service = new TenderService(tenderRepository, EmailServiceMock.Object);
 
-            Tender addedTender = service.AddTender(new Tender(TEST_ID, null, TEST_START, TEST_END, TEST_OFFER_WINNER, TEST_DELETED));
+            Tender addedTender = service.AddTender(new Tender(TEST_ID, null, null, TEST_OFFER_WINNER, TEST_DELETED));
             service.DeleteTender(TEST_ID);      //rollback
             
             addedTender.ShouldNotBeNull();
-        }*/
+        }
 
     }
 }
